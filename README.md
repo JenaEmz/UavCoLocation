7335c5fd579c254abcebc33f4a78041727ad2792
jena <csq19950714@163.com>
2019-04-19 15:34:35 +0800

    完成测试用例

---
+++ README.md
@@ -0,0 +1,7 @@
+# UavCoLocation
+
+## 用法
+
+1. 直接执行就orb.sh就行
+2. config里面有个DictPath属性，把这个属性变成你对应的位置
+3. 这个默认开启了gdb调试
---
+++ include/Tracking.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "ORBVocabulary.h"
+#include "ORBextractor.h"
+#include "ORBmatcher.h"
+#include "Frame.h"
+#include "MapPoint.h"
+
+#include "Map.h"
+#include <string>
+#include <thread>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <thread>
+using namespace std;
+
+class Map;
+
+class Tracking
+{
+  private:
+    //Calibration matrix
+    cv::Mat mK;
+    cv::Mat mDistCoef;
+    float mbf;
+    Frame mCurrentFrame;
+    Frame mLastFrame;
+    int mTrackFlag;
+    float mThDepth;
+    Map* mpMap;
+
+<<<<<<< HEAD
+    int debug;
+=======
+>>>>>>> 8736e0c5a964f99dadb8fac1b24e05251a0ddfa0
+    cv::Mat out_left_1;
+    cv::Mat out_right_1;
+    cv::Mat out_left_2;
+    cv::Mat out_right_2;
+
+    ORBVocabulary* mpVocabulary;
+
+  public:
+    void traceMap(Frame* frame);
+    void draw_match(int num, vector<int> rot, vector<MapPoint* > mvpMapPoints);
+    void StereoInitialization();
+
+    //new function
+    bool StereoInitialization(cv::Mat& Tcw);
+    //set last frame
+    bool TrackStereoBitstream(cv::Mat& Tcw, const std::vector<cv::KeyPoint> &keyPointsLeft,
+                                       const cv::Mat &descriptorLeft, const std::vector<unsigned int> &visualWords, 
+                                       const std::vector<cv::KeyPoint> &keyPointsRight, const cv::Mat &descriptorRight);
+    Tracking(const cv::FileStorage &fSettings);
+    cv::Mat GrabImageStereo(const cv::Mat &imRectLeft, const cv::Mat &imRectRight, int id);
+    bool LoopDetecting();
+    bool RelativePoseEPnp();
+    bool RelativePoseG2o();
+    bool SetCurrentFrame(const cv::Mat &imRectLeft, const cv::Mat &imRectRight);
+    cv::Mat GetRelPose();
+    ORBextractor *mpORBextractorLeft, *mpORBextractorRight;
+};
